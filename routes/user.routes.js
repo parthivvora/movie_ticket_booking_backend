cac19ddb90fd76931d58bcecf88c2fd225b9ed32
Parthiv Vora <154301292+parthivvora@users.noreply.github.com>
2024-04-19 18:22:37 +0530

    Delete routes/user.routes.js

--- routes/user.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { userRegister } = require("../controllers/user.controller");
-const router = express.Router();
-
-router.post("/register", userRegister);
-
-module.exports = router;

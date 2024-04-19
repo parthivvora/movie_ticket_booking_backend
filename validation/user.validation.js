b29871a5b5a96d96f1983bcd8cb786865d6b035c
Parthiv Vora <154301292+parthivvora@users.noreply.github.com>
2024-04-19 18:22:20 +0530

    Delete validation directory

--- validation/user.validation.js
+++
@@ -1,8 +0,0 @@
-const joi = require("joi");
-
-exports.registerValidation = joi.object({
-  name: joi.string().required(),
-  email: joi.string().email().lowercase().required(),
-  password: joi.string().alphanum().min(4).max(10).required(),
-  contact: joi.number().required(),
-});
